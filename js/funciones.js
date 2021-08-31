324a097155b1a22e1e416c67055dd455c4d55c48
Omar <omardanielesquivel@gmail.com>
2021-08-31 05:03:00 -0500

    Inicio

---
+++ js/funciones.js
@@ -0,0 +1,23 @@
+const modulo = (() =>{
+
+
+    'use strict'
+
+    const a = '                 (__) \n';
+    const b = '                 (oo) \n';
+    const c = '           /------\/ \n';
+    const d = '          / |    || \n';
+    const e = '         *  /\---/\ \n';
+    const f = '            ~~   ~~ \n';
+    const g = '..."Have you mooed today?"... \n';
+
+
+    btnBienvenidos.addEventListener('click', () => {
+
+        alert(a + b + c + d + e + f + g);
+
+    });
+
+
+
+})();
